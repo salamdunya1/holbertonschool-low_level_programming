c2462e3e5ba42b4e3a2d764a3ddf3c92c76bd984
Salam Dunya <burmurdack@gmail.com>
2024-02-20 14:26:58 +0000

    salam

---
+++ hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Print the message using puts
+    puts("Programming is like building a multilingual puzzle");
+
+    // End the program with the value 0
+    return 0;
+}
+
