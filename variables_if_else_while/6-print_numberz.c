40d88bba6fc4e3e827779a74a7b308e7d030ab2f
Salam Dunya <burmurdack@gmail.com>
2024-02-22 12:56:23 +0000

    salam

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -5,14 +5,11 @@
   *
   * Return: Always (Success)
   */
-
-int main (void)
-
-{ 
-	int(n);
+int main(void)
+{
+	int n;
 
 	for (n = '0'; n <= '9'; n++)
-
 	{
 		putchar(n);
 	}
@@ -20,5 +17,4 @@ int main (void)
 	putchar('\n');
 
 	return (0);
-
 }
