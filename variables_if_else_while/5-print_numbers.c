4b3ac2418f8e50317443f35820be6adffedf608a
Salam Dunya <burmurdack@gmail.com>
2024-02-22 12:33:31 +0000

    salam

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -5,17 +5,16 @@
   *
   * Return: Always (Success)
   */
-
 int main(void)
 {
 	char n;
 
-	for (n='0';n <='9';n++)
+	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
 	}
+
 	putchar('\n');
 
 	return (0);
-
 }
