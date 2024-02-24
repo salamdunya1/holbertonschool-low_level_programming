d746b24805a5d34580205fdb29b0e1a73cd486d7
Salam Dunya <burmurdack@gmail.com>
2024-02-24 14:38:08 +0000

    salam

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers together
+ * @a: a number
+ * @b: also a number
+ *
+ * Return: the sum of @a and @b
+ */
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+	return (n);
+}
