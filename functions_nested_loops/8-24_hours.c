90d1ae16e1e8f9f567b6ba1a3df01990f95a7e2e
Salam Dunya <burmurdack@gmail.com>
2024-02-24 17:26:43 +0000

    salam

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,12 +1,33 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * jack_bauer - prints all the minutes in a day
  */
-int main(void)
+void jack_bauer(void)
 {
-    jack_bauer();
-    return (0);
+	char hh;
+	char h;
+	char mm;
+	char m;
+
+	for (hh = '0' ; hh <= '2' ; hh++)
+	{
+		for (h = '0' ; h <= '9' ; h++)
+		{
+			if (h >= '4' && hh >= '2')
+				break;
+			for (mm = '0' ; mm <= '5' ; mm++)
+			{
+				for (m = '0' ; m <= '9' ; m++)
+				{
+					_putchar(hh);
+					_putchar(h);
+					_putchar(':');
+					_putchar(mm);
+					_putchar(m);
+					_putchar('\n');
+				}
+			}
+		}
+	}
 }
