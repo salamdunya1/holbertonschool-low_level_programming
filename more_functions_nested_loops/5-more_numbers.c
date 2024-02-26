0d1c19584e1d46651033edd5926e2da965ac687b
Salam Dunya <burmurdack@gmail.com>
2024-02-26 19:27:25 +0400

    salam

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,19 +8,21 @@
  * Return: void
  */
 
-int main()
+void more_numbers(void)
 {
-    int i, j;
-    
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j <= 14; j++) {
-            if (j >= 10) {
-                _putchar(j / 10 + '0');
-            }
-            _putchar(j % 10 + '0');
-        }
-        _putchar('\n');
-    }
-    return 0;
+	int number, tens, units, row;
 
+	for (row = 1; row <= 10; row++)
+	{
+		for (number = 0; number <= 14; number++)
+		{
+			tens = number / 10;
+			units = number % 10;
+			if (number > 9)
+				_putchar(tens + '0');
+
+			_putchar(units + '0');
+		}
+		_putchar('\n');
+	}
 }
