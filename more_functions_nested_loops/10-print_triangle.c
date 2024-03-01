a39fdb61d7a3f9a6844b7b47ad0758dd18bbc6e4
Salam Dunya <burmurdack@email.com>
2024-03-01 12:50:32 +0000

    salam

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle
+ * @size: size of the triangle
+ */
+
+void print_triangle(int size)
+{
+
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for (b = size - a; b > 0; b--)
+				_putchar(' ');
+			for (b = 0; b < a; b++)
+				_putchar('#');
+			if (a == size)
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
