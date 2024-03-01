bbdbe6e8d421f601284c3576525763b45e5b9cea
Salam Dunya <burmurdack@email.com>
2024-03-01 16:21:28 +0000

    salam

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - function that print a string in reverse
+ * @s : string to print
+ */
+
+void print_rev(char *s)
+{
+	int i, j, len;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putch'\n');
+}
