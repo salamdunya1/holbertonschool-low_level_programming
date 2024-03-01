409e3770d3da71632377839448c8d5c9b440d69f
Salam Dunya <burmurdack@email.com>
2024-03-01 16:48:12 +0000

    salam

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints one char out of 2 of a string
+ * @str: string to print char from
+ */
+
+void puts2(char *str)
+{
+	int i, j;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(str[j]);
+	}
+	_putchar ('\n');
+}
