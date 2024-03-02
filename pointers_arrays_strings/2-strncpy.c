5b53bebe3180a9b9c56c578e883d78dac947fdf1
Salam Dunya <burmurdack@gmail.com>
2024-03-02 17:19:35 +0000

    salam

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - a function that copies a string
+ * @dest: destination
+ * @src source
+ *
+ * Return: the copied string stored in the dest variable
+ */
+
+
+char *_strncpy(char *dest, char *src, int n)
+{
+    int i;
+
+    for (i = 0; i < n && src[i] != '\0'; i++)
+    {
+        dest[i] = src[i];
+    }
+
+    for (; i < n; i++)
+    {
+        dest[i] = '\0';
+    }
+
+    return dest;
+}
+
