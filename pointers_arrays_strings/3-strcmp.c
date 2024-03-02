9b147812ea5c9f91e3717a240d505d0824af0a38
Salam Dunya <burmurdack@gmail.com>
2024-03-02 17:39:32 +0000

    salam

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare 2 string
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	return (0);
+}
