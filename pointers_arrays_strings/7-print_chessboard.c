7f97c7939ecdf2c38da90f27a58bdd86fc419853
Salam Dunya <burmurdack@gmail.com>
2024-03-06 14:07:00 +0000

    salam

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - updates the value of the variable
+ * @a: the pointer points to
+ * Return: returns a result.
+ */
+
+void print_chessboard(char (*a)[8]);
+{
+	int i,j;
+
+
+	for (i = 0; i < 8; i++)
+	{
+		for (i = 0; i < 8; i++)
+			_putchar(a[i][j]);
+	       _putchar('\n');
+	}
+
+}	
+	       
