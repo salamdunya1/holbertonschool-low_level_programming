5e2805aa9305781903d6e979d20c74ea122c22f8
Salam Dunya <burmurdack@gmail.com>
2024-03-09 19:41:01 +0000

    salam

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+*main - function main
+*@argc: variable int
+*@argv: variable matriz
+*Return: zero
+*/
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc - 1);
+	(void)argv;
+	return (0);
+}
