f59b65376fca27eee2a280046af8f72dd2580247
Salam Dunya <burmurdack@gmail.com.com>
2024-03-16 15:09:40 +0000

    salam

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,45 +1,55 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "main.h"
+
 /**
-*string_nconcat - funtion concat two string
-*@s1: variable
-*@s2: variable
-*@n: variable
-*Return: char
-*/
+ * *string_nconcat - prints buffer in hexa
+ * @s1: the address of memory to print
+ * @s2: the size of the memory to print
+ * @n: number of bytes to concatenates
+ * Return: 0.
+ */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len, i;
-	unsigned int len2, j;
-	char *cat = NULL;
+	unsigned int i;
+	unsigned int len1 = 0;
+	unsigned int len2 = 0;
+	char *sconcat;
 
-	if (s1 == 0)
+	if (s1 == NULL)
 		s1 = "";
-	if (s2 == 0)
+	if (s2 == NULL)
 		s2 = "";
-	len = strlen(s1);
-	len2 = strlen(s2);
-	if (n > len2)
-		cat = malloc((len + len2) + 1);
-	else
-		cat = malloc((len + n) + 1);
-	if (cat == NULL)
+
+	for (i = 0; s1[i] != 0; i++)
+	{
+		len1++;
+	}
+	for (i = 0; s2[i] != 0; i++)
+		len2++;
+
+	sconcat = malloc(sizeof(char) * (len1 + n) + 1);
+
+	if (sconcat == NULL)
+	{
 		return (NULL);
-	i = 0;
-	while (i < len)
-	{
-		cat[i] = s1[i];
-		i++;
 	}
-	j = 0;
-	while (j < n && j < len2)
+	if (len2 <= n)
 	{
-		cat[i] = s2[j];
-		i++;
-		j++;
+		for (i = 0; s1[i] != 0; i++)
+			sconcat[i] = s1[i];
+		for (i = 0; s2[i] != 0; i++)
+			sconcat[len1 + i] = s2[i];
+		sconcat[len1 + i] = 0;
 	}
-	cat[i] = '\0';
-	return (cat);
+	else
+	{
+		for (i = 0; s1[i] != 0; i++)
+			sconcat[i] = s1[i];
+		for (i = 0; i < n; i++)
+			sconcat[len1 + i] = s2[i];
+	}
+	sconcat[len1 + i] = '\0';
+	return (sconcat);
 }
